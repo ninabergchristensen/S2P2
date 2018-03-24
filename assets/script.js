b826385e9302bd63d4785702e73773b10b9cca15
ninabergchristensen <grafik@ninachristensen.dk>
2018-03-24 20:46:18 +0100

    Tilføjet script og andre småting

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+function accordion(){
+	// her skal skrives en function, der får accordions til at expand/collapse
+	alert("Boo");
+	//var accordionThis = document.getElementById(value).parentElement;
+	//alert(accordionThis);
+	//console.log(accordionThis);
+}
